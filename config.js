de78129e6238cc007f5eb84fbdff4081d7aedca5
routefromhome-collab <routefromhome@gmail.com>
2026-09-17 14:44:25 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+window.APP_CONFIG = {
+
+    SUPABASE_URL: "https://zmsfgnffagjwavibfwgj.supabase.co",
+
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inptc2ZnbmZmYWdqd2F2aWJmd2dqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk2NDI0MzMsImV4cCI6MjEwNTIxODQzM30.n0Tdt8kvbvLfzO6-QE86sw9IG0x7M2e7ZKBYg4RrQgs"
+
+};
